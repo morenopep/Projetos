76d4571ccc6cd403404c198ad987d03cd71ff8c7
Pedro Estevão <pedrowarsps4@gmail.com>
2021-03-24 16:12:59 -0300

    Configurando a pag de inicio

--- Projeto-Blog-Pessoal/Front/blogPessoal/src/app/entrar/entrar.component.ts
+++ Projeto-Blog-Pessoal/Front/blogPessoal/src/app/entrar/entrar.component.ts
@@ -24,10 +24,12 @@ export class EntrarComponent implements OnInit {
   entrar(){
     this.auth.entrar(this.userLogin).subscribe((resp: UserLogin)=>{
       this.userLogin = resp
+
       environment.token = this.userLogin.token
       environment.nome = this.userLogin.nome
       environment.foto = this.userLogin.foto
       environment.id = this.userLogin.id
+      
       this.router.navigate(['/inicio'])    
 
     }, erro => {
